3daac3415fba88ca7f32bdd9bda4073e8919c8bc
Gustavo Benitez <gustavo.b@solunika.com>
2020-04-20 00:06:17 -0300

    - Added validation, just a fix. We should implement JOI or something

--- src/commands/listProducts/validator.js
+++ src/commands/listProducts/validator.js
@@ -1,1 +1,9 @@
-module.exports = (...params) => true
+module.exports = async (msg, ...params) => {    
+    let valid = true;
+    if(!(msg.hasOwnProperty("guild"))) {
+        msg.channel.send("No encuentro el ID de la sala, intenta dar este comando en un canal")
+        valid = false;
+    }
+
+    return valid;
+  }
